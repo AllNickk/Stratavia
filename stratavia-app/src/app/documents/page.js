762f7ef470df8d61bc9c14f7079ed12fc911aaca
Alexander Claremont-Augustus <alexanderclaremontaugustus@gmail.com>
2026-07-01 16:22:12 -0300

    style: ajusta formatação do código

---
+++ stratavia-app/src/app/documents/page.js
@@ -0,0 +1,8 @@
+export default function DocumentsPage() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1>Documentos</h1>
+      <p>Rota encontrada com sucesso.</p>
+    </div>
+  );
+}
